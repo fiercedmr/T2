8d6f2706a1a9bf1f5b24fc090be3d3f159761e35
fiercedmr <fiercedmr@gmail.com>
2026-04-16 00:49:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,21 @@
 import yfinance as yf
 import requests
+import os
+import time
 
-BOT_TOKEN = "7884471454:AAGACFUOkmhE4AN9sV3yRCtU5kypufQSqYk"
-CHAT_ID = "838128310"
+BOT_TOKEN = os.getenv("7884471454:AAGACFUOkmhE4AN9sV3yRCtU5kypufQSqYk")
+CHAT_ID = os.getenv("838128310")
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
 def run():
-    nifty = yf.download("^NSEI", period="2d", interval="1d")
-    vix = yf.download("^INDIAVIX", period="2d", interval="1d")
-
-    today = nifty.iloc[-1]
-    prev = nifty.iloc[-2]
-
-    gap = ((today['Open'] - prev['Close']) / prev['Close']) * 100
-
-    msg = f"""
-📊 NIFTY:
-Price: {round(today['Close'],2)}
-Gap: {round(gap,2)}%
-
-📉 VIX: {round(vix.iloc[-1]['Close'],2)}
-"""
-
-    send_telegram(msg)
-    def run ():
     nifty = yf.download("^NSEI", period="5d", interval="1d")
     vix = yf.download("^INDIAVIX", period="5d", interval="1d")
 
-    # Check if data is enough
     if len(nifty) < 2 or len(vix) < 1:
-        send_telegram("⚠️ Not enough data to calculate")
+        send_telegram("⚠️ Not enough data")
         return
 
     today = nifty.iloc[-1]
@@ -40,14 +23,21 @@ Gap: {round(gap,2)}%
 
     gap = ((today['Open'] - prev['Close']) / prev['Close']) * 100
 
-    msg = f"""
+    message = f"""
 📊 NIFTY:
-Price: {round(today['Close'],2)}
-Gap: {round(gap,2)}%
+Price: {round(today['Close'], 2)}
+Gap: {round(gap, 2)}%
 
-📉 VIX: {round(vix.iloc[-1]['Close'],2)}
+📉 VIX:
+{round(vix.iloc[-1]['Close'], 2)}
 """
 
-    send_telegram(msg)
+    send_telegram(message)
 
+while True:
+    try:
+        run()
+    except Exception as e:
+        send_telegram(f"❌ Error: {str(e)}")
 
+    time.sleep(300)  # runs every 5 minutes
